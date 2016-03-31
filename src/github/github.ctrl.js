fed12c9a95518df2993efed520b05cf0d469bdea
kilowatt11 <kilowatt11@cableone.net>
2016-03-30 20:45:57 -0600

    added annotate, uglify, and sourcemaps

--- src/github/github.ctrl.js
+++ src/github/github.ctrl.js
@@ -1,8 +1,7 @@
 
 angular.module('app')
 .controller('GithubCtrl', function($scope,GithubSvc){
-    
-    
+  
     GithubSvc.fetchUsers().success(function (users){
         $scope.users = users
         
